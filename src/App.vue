41e1466408eb89a51b9d1e7a6cdd764b27a28cf7
Hasan Goli <mrhasangoli@gmail.com>
2023-11-20 21:51:26 +0330

    Just finished

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,77 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <Header />
+  <div class="container">
+    <Balance :total='+total' />
+    <IncomeExpenses :income='+income' :expenses='+expenses' />
+    <TransactionList :transactions="transactions" @transactionDeleted="handleTransactionDeleted" />
+    <AddTransaction @transactionSubmitted="handleTransactionSubmitted" />
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script setup>
+import AddTransaction from './components/AddTransaction.vue';
+import Balance from './components/Balance.vue';
+import Header from './components/Header.vue';
+import IncomeExpenses from './components/IncomeExpenses.vue';
+import TransactionList from './components/TransactionList.vue';
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+import { useToast } from 'vue-toastification';
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+import { computed, onMounted, ref } from 'vue';
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+const toast = useToast();
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+const transactions = ref([]);
+
+onMounted(() => {
+  const savedTransactions = JSON.parse(localStorage.getItem('transactions')) || [];
+  transactions.value = savedTransactions;
+});
+
+// Get total
+const total = computed(() => {
+  return transactions.value.reduce((acc, transaction) => {
+    return acc + transaction.amount;
+  }, 0).toFixed(2);
+});
+
+// Get income
+const income = computed(() => {
+  return transactions.value.filter(transaction => transaction.amount > 0).reduce((acc, transaction) => {
+    return acc + transaction.amount;
+  }, 0).toFixed(2);
+});
+
+// Get expenses
+const expenses = computed(() => {
+  return transactions.value.filter(transaction => transaction.amount < 0).reduce((acc, transaction) => {
+    return acc + transaction.amount;
+  }, 0).toFixed(2);
+});
+
+// Handle transaction
+const handleTransactionSubmitted = transactionData => {
+  transactions.value.push({
+    id: new Date(),
+    text: transactionData.text,
+    amount: transactionData.amount,
+  });
+
+  saveTransactionsToLocalStorage()
+
+  toast.success('Transaction added');
+};
+
+const handleTransactionDeleted = id => {
+  transactions.value = transactions.value.filter(transaction => transaction.id !== id);
+
+  saveTransactionsToLocalStorage()
+
+  toast.success('Transaction deleted');
+};
+
+// Save to local storage
+const saveTransactionsToLocalStorage = () => {
+  localStorage.setItem('transactions', JSON.stringify(transactions.value));
+};
+</script>
